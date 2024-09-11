ec7be11a8e0bd9a80f21d500b42d03def639f298
Petr Zalizko <pzalizko@gmail.com>
2024-09-11 17:38:06 +0300

    Add project files.

---
+++ Soccer.Multilingual/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Soccer.Multilingual.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
